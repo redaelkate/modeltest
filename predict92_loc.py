9ac2218c136b11cad4c943ca46520a6aef818e69
EL-KATE REDA <146549050+redaelkate@users.noreply.github.com>
2024-11-29 20:55:35 +0100

    Update predict92_loc.py

--- predict92_loc.py
+++ predict92_loc.py
@@ -26,31 +26,31 @@ cv2.setNumThreads(0)
 cv2.ocl.setUseOpenCL(False)
 
 
-def loc_92(models,image):
+def loc_92(models,img):
 
     with torch.no_grad():
-            img = preprocess_inputs(img)
-            inp = []
-            inp.append(img)
-            inp.append(img[::-1, ...])
-            inp.append(img[:, ::-1, ...])
-            inp.append(img[::-1, ::-1, ...])
-            inp = np.asarray(inp, dtype='float')
-            inp = torch.from_numpy(inp.transpose((0, 3, 1, 2))).float()
-            inp = Variable(inp)
+        img = preprocess_inputs(img)
+        inp = []
+        inp.append(img)
+        inp.append(img[::-1, ...])
+        inp.append(img[:, ::-1, ...])
+        inp.append(img[::-1, ::-1, ...])
+        inp = np.asarray(inp, dtype='float')
+        inp = torch.from_numpy(inp.transpose((0, 3, 1, 2))).float()
+        inp = Variable(inp)
 
-            pred = []
-            for model in models:               
-                msk = model(inp)
-                msk = torch.sigmoid(msk)
-                msk = msk.cpu().numpy()        
-                pred.append(msk[0, ...])
-                pred.append(msk[1, :, ::-1, :])
-                pred.append(msk[2, :, :, ::-1])
-                pred.append(msk[3, :, ::-1, ::-1])
+        pred = []
+        for model in models:               
+            msk = model(inp)
+            msk = torch.sigmoid(msk)
+            msk = msk.cpu().numpy()        
+            pred.append(msk[0, ...])
+            pred.append(msk[1, :, ::-1, :])
+            pred.append(msk[2, :, :, ::-1])
+            pred.append(msk[3, :, ::-1, ::-1])
 
-            pred_full = np.asarray(pred).mean(axis=0) 
-            msk = pred_full * 255
-            msk = msk.astype('uint8').transpose(1, 2, 0)
+        pred_full = np.asarray(pred).mean(axis=0) 
+        msk = pred_full * 255
+        msk = msk.astype('uint8').transpose(1, 2, 0)
             # cv2.imwrite(path.join(pred_folder, '{0}.png'.format(f.replace('.png', '_part1.png'))), msk[..., 0], [cv2.IMWRITE_PNG_COMPRESSION, 9])
     return msk
