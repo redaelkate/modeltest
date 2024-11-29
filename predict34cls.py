e755a4e42d5123769c605822664083b9a354d7ac
EL-KATE REDA <146549050+redaelkate@users.noreply.github.com>
2024-11-29 23:45:13 +0100

    Update predict34cls.py

--- predict34cls.py
+++ predict34cls.py
@@ -26,51 +26,11 @@ from utils import preprocess_inputs
 cv2.setNumThreads(0)
 cv2.ocl.setUseOpenCL(False)
 
-models_folder = 'weights' #/work/
-
-if __name__ == '__main__':
-    t0 = timeit.default_timer()
-
-    seed = int(sys.argv[1])
-
-    pre_file = sys.argv[2]
-    post_file = sys.argv[3]
-    loc_pred_file = sys.argv[4]
-    cls_pred_file = sys.argv[5]
-
-    pred_folder = 'res34cls2_{}_tuned'.format(seed)
-    makedirs(pred_folder, exist_ok=True)
-
-    models = []
-
-    snap_to_load = 'res34_cls2_{}_tuned_best'.format(seed)
-
-    model = Res34_Unet_Double(pretrained=None)
-
-    model = nn.DataParallel(model)
-    
-    print("=> loading checkpoint '{}'".format(snap_to_load))
-    checkpoint = torch.load(path.join(models_folder, snap_to_load), map_location=device)
-    loaded_dict = checkpoint['state_dict']
-    sd = model.state_dict()
-    for k in model.state_dict():
-        if k in loaded_dict and sd[k].size() == loaded_dict[k].size():
-            sd[k] = loaded_dict[k]
-    loaded_dict = sd
-    model.load_state_dict(loaded_dict)
-    print("loaded checkpoint '{}' (epoch {}, best_score {})"
-            .format(snap_to_load, checkpoint['epoch'], checkpoint['best_score']))
-
-    model.eval()
-    models.append(model)
+def cls_34(models,img,img2):
 
     with torch.no_grad():
-        img = cv2.imread(pre_file, cv2.IMREAD_COLOR)
-        img2 = cv2.imread(post_file, cv2.IMREAD_COLOR)
-
         img = np.concatenate([img, img2], axis=2)
         img = preprocess_inputs(img)
-
         inp = []
         inp.append(img)
         inp.append(img[::-1, ...])
@@ -79,7 +39,6 @@ if __name__ == '__main__':
         inp = np.asarray(inp, dtype='float')
         inp = torch.from_numpy(inp.transpose((0, 3, 1, 2))).float()
         inp = Variable(inp)
-
         pred = []
         
         for model in models:
@@ -99,10 +58,6 @@ if __name__ == '__main__':
         
         msk = pred_full * 255
         msk = msk.astype('uint8').transpose(1, 2, 0)
-
-        f = os.path.basename(cls_pred_file)
-        cv2.imwrite(path.join(pred_folder, '{0}'.format(f + '_part1.png')), msk[..., :3], [cv2.IMWRITE_PNG_COMPRESSION, 9])
-        cv2.imwrite(path.join(pred_folder, '{0}'.format(f + '_part2.png')), msk[..., 2:], [cv2.IMWRITE_PNG_COMPRESSION, 9])
-
-    elapsed = timeit.default_timer() - t0
-    print('Time: {:.3f} min'.format(elapsed / 60))
+        # cv2.imwrite(path.join(pred_folder, '{0}'.format(f + '_part1.png')), msk[..., :3], [cv2.IMWRITE_PNG_COMPRESSION, 9])
+        # cv2.imwrite(path.join(pred_folder, '{0}'.format(f + '_part2.png')), msk[..., 2:], [cv2.IMWRITE_PNG_COMPRESSION, 9])
+    return msk
